497e22f1c9994f6666a6d669667b837bc2398bc3
abuhtoyarov <buhtoyarov1986@gmail.com>
2015-03-13 11:35:15 +0300

    add test Model check dependent

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
 
   it{ should validate_presence_of :title }
+
   it{ should validate_presence_of :body }
+
   it{ should have_many :answers }
+
+  it {should have_many(:answers).dependent(:destroy)}
 
 end
