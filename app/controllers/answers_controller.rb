7b57f16f0611e4a1e210c556783476efa75bfee5
abuhtoyarov <buhtoyarov1986@gmail.com>
2015-03-24 19:55:19 +0300

    Add test Answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,14 @@
 class AnswersController < ApplicationController
+
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.create(answer_params)
+    redirect_to questions_path(@question)
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,4 +2,29 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
 
+  describe 'POST #create' do
+    let(:question){ create(:question) }
+
+    context 'with valid attributes' do
+      it 'saved the new answer in the database' do
+        expect{ post :create, question_id: question, answer: attributes_for(:answer) }.to change(Answer, :count).by(1)
+      end
+      it 'redirect to show view' do
+        expect{ post :create, question_id: question, answer: attributes_for(:answer) }.to change(Answer, :count).by(1)
+        expect(response).to redirect_to questions_path(assigns(:question))
+      end
+
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the question' do
+        expect{ post :create, question_id: question, answer: attributes_for(:invalid_answer) }.to_not change(Answer, :count)
+      end
+      it 'redirect to show view' do
+        post :create, question_id: question, answer: attributes_for(:invalid_answer)
+        expect(response).to redirect_to questions_path(assigns(:question))
+      end
+    end
+  end
+
 end
