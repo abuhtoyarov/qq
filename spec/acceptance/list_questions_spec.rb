1fed0cb9c789b7bb6528630ead509c9d1b3ba0dc
abuhtoyarov <buhtoyarov1986@gmail.com>
2015-03-29 13:26:59 +0300

    Add acceptance test: List Questions

---
+++ spec/acceptance/list_questions_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'View questions', %q{
+        In order to find answer on question
+        As an any user
+        I want view lists of questions
+} do
+
+  scenario 'Any user may view the list of questions' do
+    Question.create(title: "question1", body: "question body1")
+    Question.create(title: "question2", body: "question body2")
+
+    visit questions_path
+
+    expect(page).to have_content 'question1'
+    expect(page).to have_content 'question2'
+
+    save_and_open_page
+  end
+
+end
