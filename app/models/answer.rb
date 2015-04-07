b34598638072e0ba3960b7f7a10b08bacb8b5c0c
abuhtoyarov <buhtoyarov1986@gmail.com>
2015-04-07 20:35:55 +0300

    Refactoring model and css answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,16 @@ class Answer < ActiveRecord::Base
   validates :body, presence: true
   belongs_to :question
   belongs_to :user
+  before_update :replace_best_answer
+
+  protected
+    def replace_best_answer
+      if self.best
+        Answer.where(question_id:self.question_id).each do |answer|
+          answer.update_column(:best,nil)
+        end
+      end
+    end
 end
+
+
