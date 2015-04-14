04c50701275836f08244d35fb54e005a4c1eeaf6
abuhtoyarov <buhtoyarov1986@gmail.com>
2015-04-14 11:43:52 +0300

    Add acceptance test: User may add two attachments on question

--- spec/acceptance/add_attachments_to_question_spec.rb
+++ spec/acceptance/add_attachments_to_question_spec.rb
@@ -21,4 +21,19 @@ feature 'Add attachments to question', %q{
 
     expect(page).to have_link 'rails_helper.rb'
   end
+
+    scenario 'Authenticated user (Author answer) add two attachments to question', js: true do
+      fill_in 'Title', with: 'Question title'
+      fill_in 'Body', with: 'body'
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+      click_on 'Add attachment'
+
+      next_file =  all('.new_question .fields').last
+      next_file.attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+
+      click_on 'Create question'
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+  end
 end
