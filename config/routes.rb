74d3df68c8c181392931668e0e08319063cfe0a0
abuhtoyarov <buhtoyarov1986@gmail.com>
2015-05-24 20:56:33 +0300

    configure devise and omniauth for facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controller: {omniauth_callbacks: 'omniauth_callbacks'}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
