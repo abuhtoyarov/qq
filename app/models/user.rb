350ef1223fb028ede9e771bbd7473a2d8a8c8ae6
abuhtoyarov <buhtoyarov1986@gmail.com>
2015-05-24 22:05:03 +0300

    authorization from facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,23 @@ class User < ActiveRecord::Base
   has_many :answers
   has_many :votes
   has_many :comments
-  
+  has_many :authorizations
+
+  def self.find_for_oauth(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+
+    email = auth.info['email']
+    user = User.where(email: email).first
+
+    if user
+      user.authorizations.create(provider: auth.provider, uid: auth.uid)
+    else
+      password = Devise.friendly_token[0, 20]
+      user =User.create(email: email, password: password, password_confirmation: password)
+      user.authorizations.create(provider: auth.provider, uid: auth.uid)
+    end
+
+    user
+  end
 end
