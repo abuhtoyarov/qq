93055e0763fbb2ea5dae2fbe5081664fbb21d078
abuhtoyarov <buhtoyarov1986@gmail.com>
2015-06-01 14:25:23 +0300

    add mailer send_daily_digest

---
+++ app/mailers/send_daily_digest.rb
@@ -0,0 +1,2 @@
+class SendDailyDigest < ApplicationMailer
+end
---
+++ spec/mailers/previews/send_daily_digest_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/send_daily_digest
+class SendDailyDigestPreview < ActionMailer::Preview
+
+end
