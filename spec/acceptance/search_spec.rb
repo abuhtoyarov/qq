576a664829b4e292a05b8d0564b3aa91547d47f8
abuhtoyarov <buhtoyarov1986@gmail.com>
2015-06-04 20:38:57 +0300

    Add acceptance tests

--- spec/acceptance/search_spec.rb
+++ spec/acceptance/search_spec.rb
@@ -5,11 +5,14 @@ feature 'User may search content', %q{
         As an user
         I want to be able search content
 } do
-  given!(:user) { create(:user) }
+
+  given!(:user) { create(:user, email: 'testsearch@tet.ru') }
   given!(:question) { create(:question, title: '123', body: '345') }
   given!(:answer) { create(:answer, question: question, body: 'test search') }
+  given!(:comment_answer) { create(:comment, commentable:answer, body: 'test comment for answer')}
+  given!(:comment_question) { create(:comment, commentable:question, body: 'test comment for question')}
   
-  scenario 'search selected item', js: true do
+  scenario 'search question', js: true do
     
     ThinkingSphinx::Test.run do
 	  sign_in(user)
@@ -17,9 +20,62 @@ feature 'User may search content', %q{
       select('Question', from: 'conditions')
       fill_in 'search', with: '123'
       click_on 'Search'
-    
+
 	  expect(page).to have_content  question.body
 	  expect(page).to have_content  question.title
+    end  
+  end
+
+  scenario 'search answer', js: true do
+    
+    ThinkingSphinx::Test.run do
+	  sign_in(user)
+      
+      select('Answer', from: 'conditions')
+      fill_in 'search', with: answer.body
+      click_on 'Search'
+
+	  expect(page).to have_content  answer.body
+	  expect(page).to_not have_content  question.body
+	  expect(page).to_not have_content  question.title
+	  expect(page).to_not have_content  comment_question.body
+	  expect(page).to_not have_content  comment_answer.body
+    end  
+  end 
+
+  scenario 'search comment', js: true do
+    
+    ThinkingSphinx::Test.run do
+	  sign_in(user)
+      
+      select('Comment', from: 'conditions')
+      fill_in 'search', with: 'test comment'
+      click_on 'Search'
+
+	  expect(page).to_not have_content  answer.body
+	  expect(page).to_not have_content  question.body
+	  expect(page).to_not have_content  question.title
+	  expect(page).to_not have_content  user.email
+	  expect(page).to have_content  comment_question.body
+	  expect(page).to have_content  comment_answer.body
+    end  
+  end
+
+  scenario 'search user', js: true do
+    
+    ThinkingSphinx::Test.run do
+	  sign_in(user)
+      
+      select('User', from: 'conditions')
+      fill_in 'search', with: 'testsearch'
+      click_on 'Search'
+
+	  expect(page).to_not have_content  answer.body
+	  expect(page).to_not have_content  question.body
+	  expect(page).to_not have_content  question.title
+	  expect(page).to have_content  'testsearch@tet.ru'
+	  expect(page).to_not have_content  comment_question.body
+	  expect(page).to_not	 have_content  comment_answer.body
     end  
   end
 
@@ -35,8 +91,9 @@ feature 'User may search content', %q{
 	  expect(page).to have_content  user.email
 	  expect(page).to have_content  question.title
 	  expect(page).to have_content  answer.body
+	  expect(page).to have_content  comment_question.body
+	  expect(page).to have_content  comment_answer.body
+	 
     end  
   end
-
-
 end
